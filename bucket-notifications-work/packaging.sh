a9e3b5d2afeaf70e09100f4c211e8526779d7ca7
George Puckett <puckettgw@gmail.com>
2021-10-20 18:30:29 -0400

    add packaging script

--- bucket-notifications-work/packaging.sh
+++ bucket-notifications-work/packaging.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-zip -jr restore-bucket.zip function.py
+zip -jr restore-bucket.zip function.py build.sh requirements.txt
 fission package delete --name=bucket-restore --force
-fission package create --name=bucket-restore --sourcearchive=restore-bucket.zip --env=python-faximatic
-fission function update --name restorefunction --pkg bucket-restore --entrypoint "function.main"
+fission package create --name=bucket-restore --sourcearchive=restore-bucket.zip --env=python-faximatic 
+fission function update --name restorefunction --pkg bucket-restore --entrypoint "function.main" --buildcmd "./build.sh"
