0f28e43385bcb6cce5d4b56b57338c6392cd8b10
George Puckett <puckettgw@gmail.com>
2021-10-20 18:30:13 -0400

    update bucket notifications thing

--- bucket-notifications-work/function.py
+++ bucket-notifications-work/function.py
@@ -2,6 +2,8 @@ from flask import current_app
 from flask import request
 import json 
 import re
+import os
+import boto3
 
 def restore_site(key, domain, archive_type):
     current_app.logger.info("Restoring "+archive_type+" for site: "+domain )
@@ -9,6 +11,17 @@ def restore_site(key, domain, archive_type):
       current_app.logger.info("Do databasey things here")
     if archive_type == "uploads":
       current_app.logger.info("Do uploadey things here")
+
+def delete_backup(key):
+    # some code here to delete the file from the bucket
+    aws_access_key=os.environ['aws_access_key']
+    aws_secret_key=os.environ['aws_secret_key']
+    s3 = boto3.client('s3',
+                      endpoint_url="http://rook-ceph-rgw-objectkraken.rook-ceph.svc:80",
+                      aws_access_key_id=aws_access_key,
+                      aws_secret_access_key=aws_secret_key)
+    s3.delete_object(Bucket='hostkraken-backup',Key=key)
+
 def main():
     current_app.logger.info("S3 Bucket Object Created!")
     data = request.get_data()
@@ -27,7 +40,9 @@ def main():
     current_app.logger.info("Archive type: %s" % (archive_type))
     if "invalid" != archive_type:
       restore_site(s3_key,domain,archive_type)
+      delete_backup(s3_key)
     else:
       current_app.logger.info("Invalid archive type. Doing nothing.")
+      delete_backup(s3_key)
 #    current_app.logger.info("\nBODY\n%s" % (request.get_data()))
     return "S3 Bucket Object Created!"  
